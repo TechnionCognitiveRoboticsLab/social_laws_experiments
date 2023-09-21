2673a2ab00a48cf01161c552ecb7e73e62f9d549
Erez Karpas <karpase@gmail.com>
2023-09-21 08:54:46 +0300

    run in parallel using pool

--- check_robustness.py
+++ check_robustness.py
@@ -8,18 +8,61 @@ import csv
 import create_ma_benchmarks
 import logging
 import time
-from call_function_with_timeout import SetTimeout
+from concurrent.futures import TimeoutError
+from pebble import ProcessPool, ProcessExpired
 
 
 PDDL_DOMAINS_PATH = "downward-benchmarks"
 ROBUSTNESS_RESULTS_FILE = "robustness_results.csv"
-PLANNER_TIMEOUT = 60
+PLANNER_TIMEOUT = 600
 
 random.seed(2023)
 logging.basicConfig(filename='social_law_experiments.log', encoding='utf-8', level=logging.DEBUG)
 up.shortcuts.get_environment().credits_stream = None
 
-def main():
+def check_robustness(args):
+    logging.info("Job pool started job: %s", args)
+    domain, problem_name, agent_type = args
+    reader = PDDLReader()
+    try:
+        logging.info("Reading %s:%s", domain, problem_name)
+        domain_filename = os.path.join(PDDL_DOMAINS_PATH, domain, "domain.pddl")
+        problem_filename = os.path.join(PDDL_DOMAINS_PATH, domain, problem_name)
+        problem = reader.parse_problem(domain_filename, problem_filename)
+    except Exception as e:
+        logging.error("Failed to parse problem: %s", e)
+        return -1
+    
+    logging.info("Converting to MA")
+    try:
+        samac = SingleAgentToMultiAgentConverter([agent_type])
+        samac_ret = samac.compile(problem)
+    except Exception as e:
+        logging.error("Failed to convert to MA: %s", e)
+        return -2
+
+    logging.info("Checking robustness")
+    try:
+        ma_problem = samac_ret.problem
+        slrc = SocialLawRobustnessChecker(planner_name="fast-downward")
+        
+        t1 = time.time()                
+        slrc_result = slrc.is_robust(ma_problem)
+        t2 = time.time()                       
+        
+        elapsed_time = t2 - t1
+
+        logging.info("Took %s time to get result: %s", elapsed_time, slrc_result.status.name)
+        with open(ROBUSTNESS_RESULTS_FILE, "a") as results_file:
+            print(domain, problem_name, str(elapsed_time), slrc_result.status.name, sep=",", file=results_file, flush=True)
+
+    except Exception as e:
+        logging.error("Error in robustness checking: %s", e)
+        return -3
+
+    return 0
+
+def generate_job_list():
     data = defaultdict(lambda : {})
     logging.info("Starting robustness check")
     domain_agent_types = create_ma_benchmarks.get_ma_agent_types()
@@ -32,7 +75,6 @@ def main():
             reader = csv.DictReader(csvfile)
             for row in reader:
                 data[row['domain']][row['problem']] = row
-        results_file = open(ROBUSTNESS_RESULTS_FILE, "a")    
 
     for domain in domain_agent_types.keys():
         logging.info("Starting domain %s", domain)
@@ -45,52 +87,25 @@ def main():
             if domain in data.keys() and problem_file in data[domain].keys():
                 logging.info("Problem %s:%s is already in results, skipping", domain, problem_file)
                 continue            
+            yield domain, problem_file, domain_agent_types[domain]
 
-            logging.info("Reading %s:%s", domain, problem_file)
-            reader = PDDLReader()
-            
-            problem_filename = os.path.join(PDDL_DOMAINS_PATH, domain, problem_file)
-            try:
-                problem = reader.parse_problem(domain_filename, problem_filename)
-            except Exception as e:
-                logging.error("Failed to parse problem: %s", e)
-                continue
+def main():
+    jobs = generate_job_list()
+    job_list = list(jobs)
 
-            logging.info("Converting to MA")
-            try:
-                samac = SingleAgentToMultiAgentConverter([domain_agent_types[domain]])
-                samac_ret = samac.compile(problem)
-            except Exception as e:
-                logging.error("Failed to convert to MA: %s", e)
-                continue
+    with ProcessPool() as pool:
+        future = pool.map(check_robustness, job_list, timeout=PLANNER_TIMEOUT)
 
-            logging.info("Checking robustness")
-            try:
-                ma_problem = samac_ret.problem
-                slrc = SocialLawRobustnessChecker(planner_name="fast-downward")
-                
-                def check_robustness():
-                    t1 = time.time()                
-                    slrc_result = slrc.is_robust(ma_problem)
-                    t2 = time.time()                       
-                    return t2-t1, slrc_result
-                func_with_timeout = SetTimeout(check_robustness, timeout=PLANNER_TIMEOUT)
-                is_done, is_timeout, erro_message, results = func_with_timeout()
-                
-                if is_timeout:
-                    logging.info("Timed out")
-                    print(domain, problem_file, "TO", "NA", sep=",", file=results_file, flush=True)
-                else:
-                    elapsed_time, slrc_result = results
-                    logging.info("Took %s time to get result: %s", elapsed_time, slrc_result.status.name)
-                    print(domain, problem_file, str(elapsed_time), slrc_result.status.name, sep=",", file=results_file, flush=True)
-
-            except Exception as e:
-                logging.error("Error in robustness checking: %s", e)
-                continue
-
-
-            logging.info("Success")
+        # try:
+        #     for input, result in zip(job_list, future.result()):
+        #         print(input, result)
+        # except TimeoutError as error:
+        #     print("function took longer than %d seconds" % error.args[1])
+        # except ProcessExpired as error:
+        #     print("%s. Exit code: %d" % (error, error.exitcode))
+        # except Exception as error:
+        #     print("function raised %s" % error)
+        #     print(error.traceback)  # Python's traceback of remote process
 
 if __name__ == '__main__':
     main()
